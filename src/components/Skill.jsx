b39fdc2c69fd19a827fad9bbacbc34e0dcfc4566
hyunminkim93 <rlagusals234@gmail.com>
2024-06-26 12:29:20 +0900

    Next.js

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,25 +1,23 @@
 import { skillText } from "@/constants";
 
 export default function Skill() {
-  return (
-    <section id="skill">
-    <div class="skill__inner">
-      <div class="skill__title">
-        <h2>Challenge <em>나의 도전</em></h2>
-      </div>
-      <div class="skill__desc">
-        <div>
-          {skillText.map((text , key) => (
-            <div>
-              <span>{ key + 1 }.</span>
-          <h3>{ text.title }</h3>
-          <p>{ text.desc}</p>
+    return (
+        <section id="skill">
+            <div class="skill__inner">
+                <div class="skill__title">
+                    <h2>프론트엔드를 향한 <em>나의 여정</em></h2>
+                </div>
+                <div class="skill__desc">
+                    {skillText.map((skill, index) => (
+                        <div key={index}>
+                            <span>{index + 1}</span>
+                            <h3>{skill.title}</h3>
+                            <p>{skill.desc}</p>
+                        </div>
+
+                    ))}
+                </div>
             </div>
-          ))}
-          
-        </div>
-      </div>
-    </div>
-  </section>
-  )
+        </section>
+    )
 }
